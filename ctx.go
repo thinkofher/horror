690277524f9e82cebff0f0d02d2208d83ecb4654
thinkofher <piaskupiasku@gmail.com>
2021-04-02 12:06:28 +0200

    Add doc for InternalHandler function.

--- ctx.go
+++ ctx.go
@@ -5,6 +5,26 @@ import (
 	"net/http"
 )
 
+// InternalHandler returns http middleware that can be used by popular http go
+// router libraries. Returned middleware injects given function f as new
+// default internal server error handler, that will be called when WithError
+// function will meet unknown error that doesn't satisfy horror.Error interface.
+//
+// Be aware that Adapter, no matter what, will never use registered function.
+// You have to use AdapterBuilder to overwrite default behaviour.
+func InternalHandler(f func(error, http.ResponseWriter, *http.Request)) func(http.Handler) http.Handler {
+	return func(next http.Handler) http.Handler {
+		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			newCtx := setInternalServerErrorHandler(
+				r.Context(),
+				errorHandlerFunc(f),
+			)
+			next.ServeHTTP(w, r.WithContext(newCtx))
+			return
+		})
+	}
+}
+
 func defaultInternalServerErrorHandler(e error, w http.ResponseWriter, r *http.Request) {
 	w.WriteHeader(http.StatusInternalServerError)
 	w.Write([]byte(e.Error()))
@@ -30,19 +50,4 @@ func setInternalServerErrorHandler(ctx context.Context,
 	return context.WithValue(
 		ctx, internalServerErrorHandlerKey, f,
 	)
-}
-
-func RegisterInternalServerErrorHandler(
-	f func(error, http.ResponseWriter, *http.Request),
-) func(http.Handler) http.Handler {
-	return func(next http.Handler) http.Handler {
-		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-			newCtx := setInternalServerErrorHandler(
-				r.Context(),
-				errorHandlerFunc(f),
-			)
-			next.ServeHTTP(w, r.WithContext(newCtx))
-			return
-		})
-	}
 }
