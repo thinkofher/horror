2a76dcea4d3c08c3913b9debc6710027b6fcef1a
thinkofher <piaskupiasku@gmail.com>
2021-04-02 13:58:33 +0200

    Extend horror package doc.

--- horror.go
+++ horror.go
@@ -1,5 +1,17 @@
 /*
 Package horror implements interfaces for http error handling.
+
+Horror was created to simplify error handling with standard go http.Handlers
+and give developers tools to encapsulate common error scenarios.
+
+Thanks to WithError function and Adapter type you can use horror.Handlers
+with existing go code based on http.Handler interface.
+
+If you're interested how to use this module, please see examples that are
+placed in `examples` directory.
+
+See `github.com/thinkofher/horror/status` for convenient http errors that
+satisfy Error interface from this module.
 */
 package horror
 
