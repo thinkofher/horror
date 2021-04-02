614ba0cd54a753e0c180d2eff21e5617a1d6e60c
thinkofher <piaskupiasku@gmail.com>
2021-04-02 13:58:24 +0200

    Add doc to status package.

---
+++ status/doc.go
@@ -0,0 +1,6 @@
+/*
+Package status implements Status type and factories for every http error status
+from go http standard library. Status type is compatible with horror.Error
+interface.
+*/
+package status
